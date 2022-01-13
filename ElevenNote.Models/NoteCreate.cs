804caec3687bc772cc3dc5a6b82fd7f884082ca6
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-01-13 18:18:46 -0500

    annd noteCreate and NoteListItem models. NoteService

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Title must be at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "Content cannot exceed 100 characters.")]
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteListItem //model - class that you pick and choose the properties you want from the entity.
+    {
+
+        public int NoteID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
