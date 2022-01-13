0385f2c2f1bfcdc427401fe2d54e2e27fa45d87d
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-01-13 13:13:18 -0500

    added note model

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+
+        //Validation attributes [Required], [Range], [MaxLength]...
+        // [Range(1,5, ErroMessage="please input a number between 1 and 5")]
+
+        //display attributes [Display(Name)] [Display]
+        //[Display(name = "Your note)]
+        
+        [Key]
+        public int NoteId { get; set; }
+        [Required]
+        public Guid OwenrId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? MedifiedUtc { get; set; }
+    }
+}
